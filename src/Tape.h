44b695fc8ea219927515b71730b9342c8a7b6a96
Bao Vuong <baovg.a1.k2023@gmail.com>
2025-11-03 19:35:57 -0500

    feat: definition of pseudo infinite memory tape

---
+++ src/Tape.h
@@ -0,0 +1,17 @@
+// Tape.h - psedo infinite type to represent memory
+#pragma once
+
+#include <vector>
+
+class Tape {
+private:
+    int cur_index_;
+    std::vector<int> cells_;
+
+public:
+    Tape();
+    int getCurrentCellValue() const;
+    void setCurrentCellValue(int value);
+    void shiftLeft();
+    void shiftRight();    
+};
